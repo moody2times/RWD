364794445e403085e2858a381b2f88d6e7d6e537
Uwojeya Moody Mudiaga <64225540+moody2times@users.noreply.github.com>
2020-06-11 08:36:11 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
-$("i").click(function() {
-    $("ul").slideToggle()});
+$("i").click(function () {
+    $("ul").slideToggle(100);
+});
 
+$(document).click(function (event) {
+const targetAtrribute = event.target.id;
+    if (targetAtrribute === "") {
+        const menu = $("ul").css("display");
+        if(menu === "block") {
+            $("ul").toggle();
+        }
+    }
+});
